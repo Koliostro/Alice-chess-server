dd5bb1c5d6718332f8676c547da4d3773b16d6b0
Koliostro <senior.starkoff2018@yandex.ru>
2024-05-08 20:35:09 +0300

    Reorgonize write system into functions

--- main.cpp
+++ main.cpp
@@ -23,15 +23,6 @@ void initDesk() {
   }
 }
 
-void openToWrite(std::ostream &file, uint16_t textToWrite) {
-  int8_t parts[2];
-
-  parts[0] = (textToWrite) & 0xFF;
-  parts[1] = (textToWrite >> 8) & 0xFF;
-
-  file.write((char *)parts, 2);
-}
-
 uint16_t openReadFile(std::istream &file) {
   {
     uint16_t value;
@@ -47,9 +38,19 @@ uint16_t openReadFile(std::istream &file) {
   }
 }
 
-void fileInit(std::ostream &file, uint8_t *sizeOfWord) {
+// Writing part
+
+void openToWrite(std::ostream &file, uint16_t textToWrite) {
+  int8_t parts[2];
+
+  parts[0] = (textToWrite) & 0xFF;
+  parts[1] = (textToWrite >> 8) & 0xFF;
+
+  file.write((char *)parts, 2);
+}
+
+void fileInit(std::ostream &file, uint8_t *sizeOfWord, const uint8_t version) {
   const std::string id = "pice";
-  const uint8_t version = 1;
   const uint8_t emptyData = 0;
   char * sizeWrittable = (char *) sizeOfWord;
 
@@ -70,41 +71,64 @@ bool isExist(const std::string &filepath) {
     return false;
 }
 
-int main() {
-  uint16_t write[2] = {0xFFFF, 0xFEFF};
-  uint8_t sizeOfWord = 2;
-
-  std::string name = "1";
-
-  std::fstream edit;
-
+uint8_t preapereFile(std::string filePath, std::fstream &file, uint8_t sizeOfWord, const uint8_t version) {
   uint8_t dataSize = 0;
   uint8_t curDataSize;
 
 // code for specific open
-  if(!isExist("bin/" + name + ".bin")) {
-    edit.open("bin/" + name + ".bin",std::ios_base::binary | std::fstream::out);
-    fileInit(edit, &sizeOfWord);
+  if(!isExist(filePath)) {
+    file.open(filePath,std::ios_base::binary | std::fstream::out);
+    fileInit(file, &sizeOfWord, version);
     curDataSize = 0;
   }
   else {
-    edit.open("bin/" + name + ".bin",std::ios::binary | std::fstream::in | std::fstream::out | std::ios::ate);
-    edit.seekg(5, std::ios::beg);
-    edit.read((char*) &curDataSize, 1);
-    edit.seekp(0, std::ios::end);
+    file.open(filePath,std::ios::binary | std::fstream::in | std::fstream::out | std::ios::ate);
+    file.seekg(5, std::ios::beg);
+    file.read((char*) &curDataSize, 1);
+    file.seekp(0, std::ios::end);
   }
-// ------------------------------------
+// ------------------------------------  
+  return curDataSize;
+}
+
+void endOfedit(std::fstream &file, uint8_t dataSize) {
+  file.seekp(5, std::ios::beg);
+  file.write((char *)&dataSize, 1);
+
+  file.close();
+}
+
+// end of writing part
+/*
+  TODOS:
+
+  create some classes for read and write and maybe in another file.
+
+*/
+
+int main() {
+  uint16_t write[2] = {0xFFFF, 0xFEFF};
+  uint8_t sizeOfWord = 2;
+  const uint8_t ver = 1;
+
+  std::string name = "1";
+  std::string filePath = "bin/1.bin";
+
+  std::fstream edit;
+
+  uint8_t dataSize = preapereFile(filePath, edit, sizeOfWord, ver);
+
+  // write here
+
   for (char i = 0; i < 2; i++) {
     openToWrite(edit, write[i]);
     dataSize++;
   }
+
+  // ----------------
+
+  endOfedit(edit, dataSize);
   
-  curDataSize += dataSize;
-
-  edit.seekp(5, std::ios::beg);
-  edit.write((char *)&curDataSize, 1);
-
-  edit.close();
   // In hex editor file read from right to left
 
   std::fstream outFile;
